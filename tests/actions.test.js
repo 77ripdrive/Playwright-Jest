857813f6cd83db423a699c7f79ac58f00c5187a0
GT <77ripdrive@gmail.com>
2020-12-26 23:43:52 +0300

    Fix action

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -17,7 +17,7 @@ describe('Actions at mainPage', () => {
 
     it('should be  "todos"', async() => {
         await expect(page).toEqualText('h1', "todos");
-        await page.screenshot({ path: 'artifacts/actions.png' });
+        await page.screenshot({ path: './artifacts/actions.png' });
     });
 
     afterAll(async() => {
